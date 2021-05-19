8b9068ee03336d1f37f0dac655b82e0bc4f7daa6
Ben Yeung <benjaminyeung@berkeley.edu>
2021-05-19 12:16:04 -0700

    Added simple moderation for blacklisted words

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require = require("esm")(module /*, options*/ )
 module.exports = require("./app.js")
 const tmi = require('tmi.js');
+const botconfig = require('../botconfig.json');
 
 const client = new tmi.Client({
     options: {
@@ -12,15 +13,37 @@ const client = new tmi.Client({
         secure: true
     },
     identity: {
-        username: 'bot-name',
-        password: 'oauth:my-bot-token'
+        username: botconfig.BOT_NAME, //bot name
+        password: botconfig.OAUTH_TOKEN //oauth:bot-token (see https://dev.twitch.tv/docs/authentication/getting-tokens-oauth)
     },
-    channels: ['hyperstanced']
+    channels: [botconfig.CHANNEL_NAME] //channels to join
 });
 client.connect().catch(console.error);
 client.on('message', (channel, userstate, message, self) => {
     if (self) return;
+    const greetings = ['hi!', 'hey how are you?', 'yo what\'s up', 'heya!', 'hey'];
+    const rand_greeting = greetings[Math.random() * greetings.length];
+
     if (message.toLowerCase() === '!hello') {
-        client.say(channel, `@${userstate.username}, heya!`);
+        client.say(channel, `@${userstate.username}, ${rand_greeting}`);
     }
+
+    moderateTwitchChat(userstate, message, channel);
 });
+
+function moderateTwitchChat(username, message, channel) {
+    // check message for any blacklisted words 
+    if (userstate.username === botconfig.CHANNEL_NAME) return; //ignore broadcaster messages
+    message = message.toLowerCase();
+    let wordFound = false;
+    wordFound = botconfig.BLOCKED_WORDS.some(blocked => message.includes(blocked.toLowerCase()));
+
+    if (wordFound) {
+        //notify user of deletion
+        client.say(channel, `@${username.username}, sorry! Your message was deleted as it contained a blacklisted word.`);
+
+        //delete message if found in blacklist
+        client.deletemessage(channel, userstate.id);
+    }
+
+}
